2961845517a9606fcb69272663177857ebe4841b
Dominik Birak <dombir5@gmail.com>
2023-04-07 12:14:45 +0200

    Initial project

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        User::truncate();
+        Schema::enableForeignKeyConstraints();
+        User::upsert(
+            [
+                [
+                    'name' => 'Jan',
+                    'surname' => 'Kowalski',
+                    'email' => 'kowal@gmail.com',
+                    'password' => Hash::make("kowal12@"),
+                    'role_id' => 2
+                ],
+                [
+                    'name' => 'Marek',
+                    'surname' => 'Nowak',
+                    'email' => 'nowaczek@gmail.com',
+                    'password' => Hash::make("nowak12@"),
+                    'role_id' => 1
+                ]
+            ],
+            'name'
+        );
+    }
+}
